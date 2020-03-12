45de8c1a256976087037f9fc9b4930573382753c
Ameya Daigavane <ameya.d.98@gmail.com>
2020-03-12 09:55:15 +0530

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='deepgenmodels',
+      version='0.0.1',
+      description='Deep Generative Models in PyTorch.',
+      author='Ameya Daigavane',
+      author_email='ameya.d.98@gmail.com',
+      packages=find_packages(),
+      license='LICENSE.md',
+    )
